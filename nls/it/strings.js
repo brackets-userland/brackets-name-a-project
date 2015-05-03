71ca365a1e81c41bd1e827ce0ba509253f283b25
Pete Nykanen <pete.a.nykanen@gmail.com>
2015-05-03 04:58:33 +0300

    Update IT translation

--- nls/it/strings.js
+++ nls/it/strings.js
@@ -1,5 +1,5 @@
 define({
     "PROJECT_NAME": "Nome progetto",
     "PROJECT_DIALOG_TITLE": "Nome progetto per {0}",
-    "PROJECT_BACKGROUND_COLOR": "Colore di sfondo (hex o nome)"
+    "PROJECT_BACKGROUND_COLOR": "Colore di sfondo (hex, hsla, rgba o nome)"
 });
